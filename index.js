4ded3151c2cc947ba3ebb38d023012bbb891351f
Nabajyoti Sahariah <nabajyoti@moptra.com>
2026-04-24 10:52:42 +0530

    console addition

--- index.js
+++ index.js
@@ -4,15 +4,18 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 app.get('/health', (req, res) => {
+  console.log("Calling /health")
   res.json({ status: 'ok' });
 });
 
 app.get('/v3', (req, res) => {
+  console.log("Calling /v3")
   res.json({ message: 'Welcome to Nodejs application v3 deployment' });
 });
 
 
 app.get('/', (req, res) => {
+  console.log("Calling /")
   res.json({ message: 'Welcome to Nodejs application deployment' });
 });
 
